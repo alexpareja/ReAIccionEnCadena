cdb7a80f4ae93fa3600033a7736a5033537a9437
alexpareja <aparej01@ucm.es>
2024-02-16 17:44:49 +0100

    logout funcionando

--- TFG_ReCAI/ReCAI_APP/urls.py
+++ TFG_ReCAI/ReCAI_APP/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,5 +9,7 @@ urlpatterns = [
     path('game/', views.game, name='game'),
     path('perfil_usuario/', views.perfil_usuario, name='usuario'),
     path('cambiar_contraseña/', views.cambiar_contraseña, name='cambiar_contraseña'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name= 'logout.html'), name='logout'),
+
     # Agrega más URLs según sea necesario
 ]
