e683af3c78c172d4f61f7d46bd9cf7f98bfc86ea
mbasca <100694317+mbasca@users.noreply.github.com>
2024-02-25 18:55:12 +0100

    BASES DE DATOS PARA PRUEBAS
    push de todo, por que hay que hacer migraciones

--- TFG_ReCAI/ReCAI_APP/admin.py
+++ TFG_ReCAI/ReCAI_APP/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import PalabrasEncadenadas
+from .models import EslabonCentral
+from .models import RondaFinal
 
-# Register your models here.
+class EncadenadasAdmin(admin.ModelAdmin):
+    list_display = ('tema', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6')
+
+class EslabonAdmin(admin.ModelAdmin):
+    list_display = ('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7')
+
+class FinalAdmin(admin.ModelAdmin):
+    list_display = ('p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7','p8', 'p9', 'p10', 'p11', 'p12', 'p13', 'final','pista') 
+
+admin.site.register(PalabrasEncadenadas, EncadenadasAdmin)
+admin.site.register(EslabonCentral, EslabonAdmin)
+admin.site.register(RondaFinal, FinalAdmin)
+
+
+
--- TFG_ReCAI/ReCAI_APP/models.py
+++ TFG_ReCAI/ReCAI_APP/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+class PalabrasEncadenadas(models.Model):
+    tema = models.CharField(max_length=100);
+    p1 = models.CharField(max_length=20);
+    p2 = models.CharField(max_length=20);
+    p3 = models.CharField(max_length=20);
+    p4 = models.CharField(max_length=20);
+    p5 = models.CharField(max_length=20);    
+    p6 = models.CharField(max_length=20);
+
+    def __str__(self):
+            return self.tema
+    
+class EslabonCentral(models.Model):
+    p1 = models.CharField(max_length=20);
+    p2 = models.CharField(max_length=20);
+    p3 = models.CharField(max_length=20);
+    p4 = models.CharField(max_length=20);
+    p5 = models.CharField(max_length=20);    
+    p6 = models.CharField(max_length=20);
+    p7 = models.CharField(max_length=20);
+
+    def __str__(self):
+            return self.p1
+    
+class RondaFinal(models.Model):
+    p1 = models.CharField(max_length=20);
+    p2 = models.CharField(max_length=20);
+    p3 = models.CharField(max_length=20);
+    p4 = models.CharField(max_length=20);
+    p5 = models.CharField(max_length=20);    
+    p6 = models.CharField(max_length=20);
+    p7 = models.CharField(max_length=20);
+    p8 = models.CharField(max_length=20);
+    p9 = models.CharField(max_length=20);
+    p10 = models.CharField(max_length=20);
+    p11 = models.CharField(max_length=20);
+    p12 = models.CharField(max_length=20);    
+    p13 = models.CharField(max_length=20);
+    final = models.CharField(max_length=20);
+    pista = models.CharField(max_length=20);
+
+    def __str__(self):
+            return self.p2
+
 # Create your models here.
